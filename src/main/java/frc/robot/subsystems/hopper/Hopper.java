65dab19b57fe051e242c029c77579e3fd5c7ccb9
PeterTomasini <peter.tomasini.stu@cedarpark.org>
2026-01-17 12:58:21 -0800

    Added hopper subsystem

--- src/main/java/frc/robot/subsystems/hopper/Hopper.java
+++ src/main/java/frc/robot/subsystems/hopper/Hopper.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.hopper;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
 
 public class Hopper extends SubsystemBase {
     private final HopperIO io;
--- src/main/java/frc/robot/subsystems/hopper/HopperIO.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperIO.java
@@ -1,13 +1,9 @@
 package frc.robot.subsystems.hopper;
 
 public interface HopperIO {
-    default void setVoltage(double voltage) {
-    }
+    default void setVoltage(double voltage) {}
 
-    default void stop() {
-        setVoltage(0.0);
-    }
+    default void stop() {setVoltage(0.0);}
 
-    default void updateInputs(HopperInputs inputs) {
-    }
+    default void updateInputs(HopperInputs inputs) {}
 }
