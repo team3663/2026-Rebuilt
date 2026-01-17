1127db84db07baa2e6dd82eec8ddc48a74f4df63
PeterTomasini <peter.tomasini.stu@cedarpark.org>
2026-01-17 14:59:13 -0800

    added controller control
    b runs at 3v while held, y stops the motor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,14 +9,10 @@ package frc.robot;
 
 import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.hardware.TalonFX;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.DriveCommands;
@@ -209,6 +205,9 @@ public class RobotContainer {
         controller.a().onTrue(intake.stopIntake());
         controller.x().whileTrue(intake.intakeWithVoltage(3.0));
 
+        //Hopper Controls
+        controller.b().whileTrue(hopper.withVoltage(3));
+        controller.y().onTrue(hopper.stop());
     }
 
     /**
