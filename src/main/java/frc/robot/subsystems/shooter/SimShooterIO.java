5ccea8569dcafd498898d602a86385730207f56f
Adriel <stammler.adriel@gmail.com>
2026-01-24 11:57:23 -0800

    Fixing bugs / Requested Changes

--- src/main/java/frc/robot/subsystems/shooter/SimShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/SimShooterIO.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.shooter;
 
-import com.ctre.phoenix6.hardware.CANcoder;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
@@ -18,12 +17,9 @@ public class SimShooterIO implements ShooterIO {
     private static final double SHOOTER_GEAR_RATIO = 1.0;
     private static final double STDEV = 0.1;
 
-    private final DCMotor hoodMotor = DCMotor.getFalcon500Foc(16);
-    private final DCMotor turretMotor = DCMotor.getFalcon500Foc(17);
-    private final CANcoder turretCanCoder1 = new CANcoder(7);
-    private final CANcoder turretCanCoder2 = new CANcoder(8);
-    private final DCMotor shooterMotor = DCMotor.getFalcon500Foc(18);
-    private final DCMotor shooterMotor2 = DCMotor.getFalcon500Foc(19);
+    private final DCMotor hoodMotor = DCMotor.getFalcon500Foc(1);
+    private final DCMotor turretMotor = DCMotor.getFalcon500Foc(1);
+    private final DCMotor shooterMotors = DCMotor.getFalcon500Foc(2);
 
     private final DCMotorSim hoodSim = new DCMotorSim(LinearSystemId.createDCMotorSystem(hoodMotor, 0.01, HOOD_GEAR_RATIO), hoodMotor, STDEV);
 
@@ -33,7 +29,7 @@ public class SimShooterIO implements ShooterIO {
 
     private final ProfiledPIDController turretController = new ProfiledPIDController(1.0, 0.0, 0.0, new TrapezoidProfile.Constraints(Units.rotationsPerMinuteToRadiansPerSecond(500.0), Units.rotationsPerMinuteToRadiansPerSecond(700.0)));
 
-    private final FlywheelSim shooterSim = new FlywheelSim(LinearSystemId.createFlywheelSystem(shooterMotor, 0.1, SHOOTER_GEAR_RATIO), shooterMotor, STDEV);
+    private final FlywheelSim shooterSim = new FlywheelSim(LinearSystemId.createFlywheelSystem(shooterMotors, 0.1, SHOOTER_GEAR_RATIO), shooterMotors, STDEV);
 
     private final ProfiledPIDController shooterController = new ProfiledPIDController(1.0, 0.0, 0.0, new TrapezoidProfile.Constraints(Units.rotationsPerMinuteToRadiansPerSecond(500.0), Units.rotationsPerMinuteToRadiansPerSecond(700.0)));
 
