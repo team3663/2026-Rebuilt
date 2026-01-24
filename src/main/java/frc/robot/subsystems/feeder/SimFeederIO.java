b42cd219ab05414d3d724d047735be149d40a831
Grayson Gile <grayson.gile@hotmail.com>
2026-01-24 14:33:11 -0800

    hardware and targetvoltage/velocity
    i added the two override methods, removed the hardware, and reformatted the code

--- src/main/java/frc/robot/subsystems/feeder/SimFeederIO.java
+++ src/main/java/frc/robot/subsystems/feeder/SimFeederIO.java
@@ -1,8 +1,5 @@
 package frc.robot.subsystems.feeder;
 
-import com.ctre.phoenix6.hardware.CANrange;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.sim.CANrangeSimState;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
@@ -11,32 +8,25 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import frc.robot.Robot;
 
-public class SimFeederIO implements FeederIO{
+public class SimFeederIO implements FeederIO {
 
-    private final TalonFX motor1; //14;
-    private final TalonFX motor2; //15;
-    private final DCMotorSim sim = new DCMotorSim(
+
+    private static final DCMotorSim sim = new DCMotorSim(
             LinearSystemId.createDCMotorSystem(DCMotor.getKrakenX60(1),
                     0.001, 1.0),
             DCMotor.getKrakenX60(1).withReduction(1.0));
 //    private final CANrangeSimState CANrangeSim = new CANrangeSimState();
-    private double targetVelocity = Double.NaN;
-    private double targetFeederVoltage = Double.NaN;
-    public SimFeederIO(TalonFX motor1, TalonFX motor2) {
-        this.motor1 = motor1;
-        this.motor2 = motor2;
-
-    }
-
     public final ProfiledPIDController controller = new ProfiledPIDController(
             1.0, 0.0, 0.0, new TrapezoidProfile.Constraints(
             Units.rotationsPerMinuteToRadiansPerSecond(500),
             Units.rotationsPerMinuteToRadiansPerSecond(700)
     )
     );
+    private double targetVelocity = Double.NaN;
+    private double targetFeederVoltage = Double.NaN;
 
     @Override
-    public void updateInputs(FeederInputs inputs){
+    public void updateInputs(FeederInputs inputs) {
         double feederVoltage = 0.0;
         if (Double.isFinite(targetVelocity)) {
             feederVoltage = controller.calculate(sim.getAngularVelocityRadPerSec(), targetVelocity);
@@ -51,4 +41,13 @@ public class SimFeederIO implements FeederIO{
         inputs.currentVelocity = sim.getAngularVelocityRadPerSec();
     }
 
+    @Override
+    public void setTargetVoltage(double voltage) {
+        targetFeederVoltage = voltage;
+    }
+
+    @Override
+    public void setTargetVelocity(double velocity) {
+        targetVelocity = velocity;
+    }
 }
