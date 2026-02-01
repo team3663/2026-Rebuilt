fd40cc736ff757ef6002af8bc1c76f8960144ff1
EllaNorby <ella.norby.stu@cedarpark.org>
2026-01-31 21:27:10 -0800

    Auto Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,6 +11,7 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotBase;
 
@@ -24,16 +25,60 @@ public final class Constants {
     public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
 
     public static final Boolean IS_ANDYMARK = false;
-    // TODO get 2026 field when available
     public static AprilTagFieldLayout FIELD =
-            AprilTagFieldLayout.loadField(IS_ANDYMARK ? AprilTagFields.k2025ReefscapeAndyMark : AprilTagFields.k2025ReefscapeWelded);
+            AprilTagFieldLayout.loadField(IS_ANDYMARK ? AprilTagFields.k2026RebuiltAndymark : AprilTagFields.k2026RebuiltWelded);
 
-    public static final Pose2d RED_AUTO_STARTING_6FT = new Pose2d(Units.inchesToMeters(156.61),
-            Units.inchesToMeters(72.0 + 14.0), Rotation2d.fromDegrees(0));
-    public static final Pose2d RED_AUTO_STARTING_CENTER = new Pose2d(Units.inchesToMeters(156.61),
-            Units.inchesToMeters(FIELD.getFieldWidth() / 2.0), Rotation2d.fromDegrees(0));
+    // Blue alliance auto starting positions
+    public static final Pose2d BLUE_LEFT_AUTO_LINE = FIELD.getTagPose(23).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters((47.0 / 2) + 13.0), 0.0, new Rotation2d(Units.degreesToRadians(180))));
+    public static final Pose2d BLUE_RIGHT_AUTO_LINE = FIELD.getTagPose(28).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters((47.0 / 2) + 13.0), 0.0, new Rotation2d(Units.degreesToRadians(180))));
+    public static final Pose2d BLUE_LEFT_UNDER_TRENCH_AUTO_LINE = FIELD.getTagPose(23).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters((47.0 / 2) - 13.0), 0.0, new Rotation2d(Units.degreesToRadians(180))));
+    public static final Pose2d BLUE_RIGHT_UNDER_TRENCH_AUTO_LINE = FIELD.getTagPose(28).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters((47.0 / 2) - 13.0), 0.0, new Rotation2d(Units.degreesToRadians(180))));
+    // TODO do I want a center starting position?
 
+    // Red Alliance auto starting positions
+    public static final Pose2d RED_LEFT_AUTO_LINE = FIELD.getTagPose(7).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters((47.0 / 2) + 13.0), 0.0, new Rotation2d(0)));
+    public static final Pose2d RED_RIGHT_AUTO_LINE = FIELD.getTagPose(12).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters((47.0 / 2) + 13.0), 0.0, new Rotation2d(0)));
+    public static final Pose2d RED_LEFT_UNDER_TRENCH_AUTO_LINE = FIELD.getTagPose(7).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters((47.0 / 2) - 13.0), 0.0, new Rotation2d(0)));
+    public static final Pose2d RED_RIGHT_UNDER_TRENCH_AUTO_LINE = FIELD.getTagPose(12).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters((47.0 / 2) - 13.0), 0.0, new Rotation2d(0)));
 
+    // TODO figure out distance away from outpost - currently set to 2 in
+    // Outpost and Depot Positions
+    public static final Pose2d BLUE_OUTPOST_CENTERED = FIELD.getTagPose(29).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters(13.0 + 2.0), 0.0, new Rotation2d(Units.degreesToRadians(180))));
+    public static final Pose2d RED_OUTPOST_CENTERED = FIELD.getTagPose(13).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters(13.0 + 2.0), 0.0, new Rotation2d(Units.degreesToRadians(180))));
+    public static final Pose2d BLUE_RIGHT_DEPOT = new Pose2d((FIELD.getFieldWidth() / 2.0) + Units.inchesToMeters(75.93) + Units.inchesToMeters(42.0 / 2.0),
+            Units.inchesToMeters(27.0 / 2.0), new Rotation2d(270));
+    public static final Pose2d BLUE_LEFT_DEPOT = new Pose2d((FIELD.getFieldWidth() / 2.0) + Units.inchesToMeters(75.93) - Units.inchesToMeters(42.0 / 2.0),
+            Units.inchesToMeters(27.0 / 2.0), new Rotation2d(270));
+    public static final Pose2d RED_RIGHT_DEPOT = new Pose2d((FIELD.getFieldWidth() / 2.0) - Units.inchesToMeters(75.93) - Units.inchesToMeters(42.0 / 2.0),
+            Units.inchesToMeters(27.0 / 2.0), new Rotation2d(90));
+    public static final Pose2d RED_LEFT_DEPOT = new Pose2d((FIELD.getFieldWidth() / 2.0) - Units.inchesToMeters(75.93) + Units.inchesToMeters(42.0 / 2.0),
+            Units.inchesToMeters(27.0 / 2.0), new Rotation2d(90));
+
+    // Climb Positions
+    public static final Pose2d BLUE_LEFT_RUNG_CLIMB = FIELD.getTagPose(31).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters(43.510 + 13.0 + 2.0), -Units.inchesToMeters(32.25 + (1.5 / 2.0)), new Rotation2d(Units.degreesToRadians(0))));
+    public static final Pose2d BLUE_RIGHT_RUNG_CLIMB = FIELD.getTagPose(31).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters(43.510 + 13.0 + 2.0), Units.inchesToMeters(32.25 + (1.5 / 2.0)), new Rotation2d(Units.degreesToRadians(0))));
+    public static final Pose2d RED_LEFT_RUNG_CLIMB = FIELD.getTagPose(15).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters(43.510 + 13.0 + 2.0), Units.inchesToMeters(32.25 + (1.5 / 2.0)), new Rotation2d(Units.degreesToRadians(180))));
+    public static final Pose2d RED_RIGHT_RUNG_CLIMB = FIELD.getTagPose(15).get().toPose2d().plus(
+            new Transform2d(Units.inchesToMeters(43.510 + 13.0 + 2.0), -Units.inchesToMeters(32.25 + (1.5 / 2.0)), new Rotation2d(Units.degreesToRadians(180))));
+
+    // Neutral Zone Positions
+    public static final Pose2d BLUE_RIGHT_CENTER_LINE = new Pose2d((FIELD.getFieldLength() / 2.0), FIELD.getTagPose(17).get().getY(), new Rotation2d(Units.degreesToRadians(90)));
+    public static final Pose2d BLUE_LEFT_CENTER_LINE = new Pose2d((FIELD.getFieldLength() / 2.0), FIELD.getTagPose(22).get().getY(), new Rotation2d(Units.degreesToRadians(90)));
+    public static final Pose2d RED_RIGHT_CENTER_LINE = new Pose2d((FIELD.getFieldWidth() / 2.0), FIELD.getTagPose(1).get().getY(),new Rotation2d(Units.degreesToRadians(270)));
+    public static final Pose2d RED_LEFT_CENTER_LINE = new Pose2d((FIELD.getFieldLength() / 2.0), FIELD.getTagPose(6).get().getY(), new Rotation2d(Units.degreesToRadians(270)));
 
     public static enum Mode {
         /**
